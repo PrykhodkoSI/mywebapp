d0c939bbdd4474f08fde0aac857963da4891f305
stas <prykhodko.s.i@gmail.com>
2017-03-27 01:38:25 +0300

    27 hw

---
+++ src/main/java/org/itstep/myWebApp/web/rest/MailRestController.java
@@ -0,0 +1,15 @@
+package org.itstep.myWebApp.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by stas on 19.03.17.
+ */
+
+@RestController
+@RequestMapping(value = "/rest/mails")
+public class MailRestController {
+
+
+}
--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,17 +1,18 @@
-use mailDb
+--use mailDb;
 
-DROP TABLE Users
+DROP TABLE Users;
 --DELETE FROM Users
 
 CREATE TABLE Users (
-  Id int IDENTITY PRIMARY KEY NOT NULL,
+  Id serial PRIMARY KEY NOT NULL, --serial
   Name NVARCHAR(255),
   LastName NVARCHAR(255),
   City NVARCHAR(255),
   Email VARCHAR(255)
 );
 
-INSERT INTO Users (Name, LastName, City, Email) VALUES ('stas1','Pr','City','Email@mail')
-INSERT INTO Users (Name, LastName, City, Email) VALUES ('Stas2','Pr','City','Email@mail')
-INSERT INTO Users (Name, LastName, City, Email) VALUES ('Stas3','Pr','City','Email@mail')
+INSERT INTO Users (Name, LastName, City, Email) VALUES
+  ('stas1','Pr','City','Email@mail'),
+  ('Stas2','Pr','City','Email@mail'),
+  ('Stas3','Pr','City','Email@mail');
 
